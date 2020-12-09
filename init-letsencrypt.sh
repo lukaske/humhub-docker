416c9a21cd0bef7876b98b4d87518bb20790899f
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2020-12-09 03:18:32 +0100

    again

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(f.kuzek.si www.f.kuzek.si)
+domains=(festival.kuzek.si www.festival.kuzek.si)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="luka@jabuk.xyz" # Adding a valid address is strongly recommended
