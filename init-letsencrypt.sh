e3d3fde0954c8292ca28d29f4550efa6b7a4fc82
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2020-12-11 00:57:12 +0100

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,6 +9,7 @@ domains=(fest.kuzek.si)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="luka@jabuk.xyz" # Adding a valid address is strongly recommended
+
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
